f10e0e35652e106a3aa4d0a6ad03831294c28887
Tom Kranz <tom@tpmkranz.org>
2015-11-20 09:22:28 +0100

    Complete testing settings technically
    But not all that user friendly...

--- app/src/main/java/org/tpmkranz/smsforward/SetupActivity.java
+++ app/src/main/java/org/tpmkranz/smsforward/SetupActivity.java
@@ -8,14 +8,17 @@ import android.content.pm.PackageManager;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.EditText;
+import android.widget.TextView;
 
 import java.security.Security;
+import java.util.HashMap;
 
 
 public class SetupActivity extends AppCompatActivity {
@@ -32,6 +35,7 @@ public class SetupActivity extends AppCompatActivity {
     private FloatingActionButton fab;
     private EditText inputEmail, inputPassword, inputServer, inputPort, inputTarget, inputPubkey;
     private SharedPreferences settings;
+    private HashMap<String,String> currentSettings;
 
 
     static {
@@ -52,11 +56,13 @@ public class SetupActivity extends AppCompatActivity {
         inputTarget = (EditText) findViewById(R.id.input_target);
         inputPubkey = (EditText) findViewById(R.id.input_pubkey);
         settings = this.getSharedPreferences(SHAREDPREFSNAME, Context.MODE_PRIVATE);
-        inputEmail.setText(settings.getString(SHAREDPREFSEMAIL, ""));
-        inputServer.setText(settings.getString(SHAREDPREFSSERVER, ""));
-        inputPort.setText(settings.getString(SHAREDPREFSPORT, ""));
-        inputTarget.setText(settings.getString(SHAREDPREFSTARGET, ""));
-        inputPubkey.setText(settings.getString(SHAREDPREFSPUBKEY, ""));
+        currentSettings = new HashMap<>();
+        readSettings(true);
+        inputEmail.setText(currentSettings.get(SHAREDPREFSEMAIL));
+        inputServer.setText(currentSettings.get(SHAREDPREFSSERVER));
+        inputPort.setText(currentSettings.get(SHAREDPREFSPORT));
+        inputTarget.setText(currentSettings.get(SHAREDPREFSTARGET));
+        inputPubkey.setText(currentSettings.get(SHAREDPREFSPUBKEY));
         receiver = new ComponentName(this, SMSListener.class);
         pm = getPackageManager();
     }
@@ -70,14 +76,23 @@ public class SetupActivity extends AppCompatActivity {
         switchUIStates(enabled);
     }
 
+    private void readSettings(boolean fromPrefs){
+        currentSettings.put(SHAREDPREFSEMAIL, fromPrefs ? settings.getString(SHAREDPREFSEMAIL, "") : inputEmail.getText().toString());
+        currentSettings.put(SHAREDPREFSPASSWORD, fromPrefs || inputPassword.getText().toString().isEmpty() ? settings.getString(SHAREDPREFSPASSWORD, "") : inputPassword.getText().toString());
+        currentSettings.put(SHAREDPREFSSERVER, fromPrefs ? settings.getString(SHAREDPREFSSERVER, "") : inputServer.getText().toString());
+        currentSettings.put(SHAREDPREFSPORT, fromPrefs ? settings.getString(SHAREDPREFSPORT, "") : inputPort.getText().toString());
+        currentSettings.put(SHAREDPREFSTARGET, fromPrefs ? settings.getString(SHAREDPREFSTARGET, "") : inputTarget.getText().toString());
+        currentSettings.put(SHAREDPREFSPUBKEY, fromPrefs ? settings.getString(SHAREDPREFSPUBKEY, "") : inputPubkey.getText().toString());
+    }
+
     private boolean haveSettingsChanged(){
-        return !getInputText(inputEmail).equals(settings.getString(SHAREDPREFSEMAIL, ""))
-                || (!getInputText(inputPassword).equals(settings.getString(SHAREDPREFSPASSWORD, ""))
+        return !getInputText(inputEmail).equals(currentSettings.get(SHAREDPREFSEMAIL))
+                || (!getInputText(inputPassword).equals(currentSettings.get(SHAREDPREFSPASSWORD))
                 && !getInputText(inputPassword).equals(""))
-                || !getInputText(inputServer).equals(settings.getString(SHAREDPREFSSERVER, ""))
-                || !getInputText(inputPort).equals(settings.getString(SHAREDPREFSPORT, ""))
-                || !getInputText(inputTarget).equals(settings.getString(SHAREDPREFSTARGET, ""))
-                || !getInputText(inputPubkey).equals(settings.getString(SHAREDPREFSPUBKEY, ""));
+                || !getInputText(inputServer).equals(currentSettings.get(SHAREDPREFSSERVER))
+                || !getInputText(inputPort).equals(currentSettings.get(SHAREDPREFSPORT))
+                || !getInputText(inputTarget).equals(currentSettings.get(SHAREDPREFSTARGET))
+                || !getInputText(inputPubkey).equals(currentSettings.get(SHAREDPREFSPUBKEY));
     }
 
     private boolean areSettingsValid(){
@@ -134,13 +149,13 @@ public class SetupActivity extends AppCompatActivity {
     public void enableBroadcastReceiver(View view) {
         if (areSettingsValid()) {
             if (haveSettingsChanged()) {
-                askUserToTestSettings();
-                // TODO write only on successful testing
                 writeSettings();
+                askUserToTestSettings();
+            }else{
+                pm.setComponentEnabledSetting(receiver,
+                        (enabled ? PackageManager.COMPONENT_ENABLED_STATE_DISABLED : PackageManager.COMPONENT_ENABLED_STATE_ENABLED),
+                        PackageManager.DONT_KILL_APP);
             }
-            pm.setComponentEnabledSetting(receiver,
-                    (enabled ? PackageManager.COMPONENT_ENABLED_STATE_DISABLED : PackageManager.COMPONENT_ENABLED_STATE_ENABLED),
-                    PackageManager.DONT_KILL_APP);
             enabled = isListenerListening(receiver);
             switchUIStates(enabled);
         }
@@ -156,12 +171,30 @@ public class SetupActivity extends AppCompatActivity {
         editor.putString(SHAREDPREFSTARGET, getInputText(inputTarget));
         editor.putString(SHAREDPREFSPUBKEY, getInputText(inputPubkey));
         editor.apply();
+        readSettings(false);
     }
 
     private void askUserToTestSettings() {
+        Snackbar
+                .make(findViewById(R.id.setup_layout), R.string.snackbar_settings_changed, Snackbar.LENGTH_INDEFINITE)
+                .setAction(R.string.snackbar_settings_confirmed, new SnackBarSettingsConfirmationListener())
+                .show();
         Intent testMailIntent = new Intent(this, EmailSenderService.class);
         testMailIntent.putExtra(EmailSenderService.INTENTEXTRASUBJECT, getResources().getString(R.string.email_subject_test));
         testMailIntent.putExtra(EmailSenderService.INTENTEXTRABODY, getResources().getString(R.string.email_body_test));
         startService(testMailIntent);
+    }
+
+    public void expandIntro(View view) {
+        TextView intro = (TextView) view;
+        intro.setSingleLine(intro.getMaxLines() != 1);
+    }
+
+    private class SnackBarSettingsConfirmationListener implements View.OnClickListener{
+
+        @Override
+        public void onClick(View v) {
+            enableBroadcastReceiver(v);
+        }
     }
 }
